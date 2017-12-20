4754e0972e2f0d58d59ed6c211d695c1e9b71b56
Vu Duc Tuyen <tuyen@naxam.net>
2017-12-20 16:55:41 +0700

    Add frameworks

---
+++ Naxam.MapboxNavigation.iOS/Structs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MapboxNavigation
+{
+    [Native]
+    public enum ManeuversStyleKitResizingBehavior : long
+    {
+        AspectFit = 0,
+        AspectFill = 1,
+        Stretch = 2,
+        Center = 3
+    }
+
+    [Native]
+    public enum MBStyleType : long
+    {
+        DayStyle = 0,
+        NightStyle = 1
+    }
+
+    [Native]
+    public enum MBFeedbackSource : long
+    {
+        User = 0,
+        Reroute = 1,
+        Unknown = 2
+    }
+
+    [Native]
+    public enum MBFeedbackType : long
+    {
+        General = 0,
+        Accident = 1,
+        Hazard = 2,
+        RoadClosed = 3,
+        NotAllowed = 4,
+        MissingRoad = 5,
+        MissingExit = 6,
+        RoutingError = 7,
+        ConfusingInstruction = 8,
+        ReportTraffic = 9
+    }
+}
---
+++ demo/NavigationQs/ViewController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+using UIKit;
+using Mapbox.Directions;
+//using Naxam.MapboxNavigation.iOS;
+using CoreLocation;
+using Mapbox;
+
+namespace NavigationQs
+{
+    public partial class ViewController : UIViewController
+    {
+        MGLMapView mapView;
+        UIButton routingBtn;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            mapView = new MGLMapView();
+            View.AddSubview(mapView);
+
+            routingBtn = new UIButton(UIButtonType.Custom);
+            routingBtn.SetTitle("Start", UIControlState.Normal);
+            routingBtn.AddTarget(DidTapOnRoutingBtn, UIControlEvent.TouchUpInside);
+
+                        
+         
+
+//Directions.shared.calculate(options) {
+//                (waypoints, routes, error) in
+//    guard let route = routes?.first else { return }
+
+//                let viewController = NavigationViewController(for: route)
+//                    self.present(viewController, animated: true, completion: nil)
+//}
+        }
+
+        private void DidTapOnRoutingBtn(object sender, EventArgs e)
+        {
+            var origin = new MBWaypoint(new CLLocationCoordinate2D(38.9131752, -77.0324047), 
+            -1, 
+            "Mapbox");
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+           
+            //var destination = new MBWaypoint(new CLLocationCoordinate2D(38.8977, -77.0365),
+                                        //-1,
+                                             //"White House");
+            //var options = new MBNavigationRouteOptions(
+            //    new MBWaypoint[] { origin, destination },
+            //    MBDirectionsProfileIdentifier.AutomobileAvoidingTraffic
+            //);
+            //MBDirections.SharedDirections.CalculateDirectionsWithOptions(
+            //options,
+            //(waypoints, route, error) =>
+            //{
+
+            //});
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            mapView.Frame = View.Bounds;
+            routingBtn.Frame = new CoreGraphics.CGRect(View.Bounds.Size.Width - 100,
+                                                       View.Bounds.Size.Height - 60,
+                                                      80,
+                                                       40);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
