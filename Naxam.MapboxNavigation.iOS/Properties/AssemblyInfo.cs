c627a3b0067d6ea0e8c04e9f630b7262b4cd0b15
Vu Duc Tuyen <tuyen@naxam.net>
2018-01-01 00:32:32 +0700

    Correct version number and assembly details

--- Naxam.MapboxNavigation.iOS/Properties/AssemblyInfo.cs
+++ Naxam.MapboxNavigation.iOS/Properties/AssemblyInfo.cs
@@ -13,19 +13,19 @@ using Foundation;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Naxam.MapboxNavigation.iOS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Xamarin.iOS binding library - Mapbox Navigation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("(c) Ha Do")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("NAXAM CO.,LTD")]
+[assembly: AssemblyProduct("XBindings")]
+[assembly: AssemblyCopyright("COPYRIGHT (c) 2018 NAXAM")]
+[assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.11.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
